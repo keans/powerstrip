5890717241c3d12d5df45a8479f07f9f9157f50c
Ansgar Kellner <keans@gmx.de>
2023-01-13 10:03:33 +0100

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_long_description():
 
 setup(
     name="python-powerstrip",
-    version="0.0.3",
+    version="0.0.4",
     description="Simple module to manage plugins.",
     long_description=get_long_description(),
     url="https://github.com/keans/powerstrip",
